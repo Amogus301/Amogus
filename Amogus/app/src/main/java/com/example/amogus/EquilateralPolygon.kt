97a3970ac94f351d45e46a8e2a1d4ad82ddc70cf
NattyEddy <99678920+NattyEddy@users.noreply.github.com>
2026-09-25 00:12:31 -0600

    Added color attribute.

--- Amogus/app/src/main/java/com/example/amogus/EquilateralPolygon.kt
+++ Amogus/app/src/main/java/com/example/amogus/EquilateralPolygon.kt
@@ -3,4 +3,5 @@ package.com.example.amogus
 class EquilateralPolygon(x: Int, y: Int, numSides: Int, length: Int): Shape(x,y) {
     private int _numSides = numSides
     private int _length = length
+    private val color: String = blue
 }
