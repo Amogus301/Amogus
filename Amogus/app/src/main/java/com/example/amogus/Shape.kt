4ee2959542fe9cb1a89ea977cf5dde37677050ab
mo <m.adetunji2@share.epsb.ca>
2026-09-25 16:39:56 -0600

    Add color field to Shape

--- Amogus/app/src/main/java/com/example/amogus/Shape.kt
+++ Amogus/app/src/main/java/com/example/amogus/Shape.kt
@@ -1,6 +1,7 @@
 package com.example.amogus
 
-abstract class Shape(val x: Int, val y: Int, val color: String = "blue") {
+abstract class Shape(val x: Int, val y: Int, val color: String = "blue")
+{
 val color: String = "Transperent"
 
 }
